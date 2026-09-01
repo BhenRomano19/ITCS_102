7d92a024266ac286f99890af44160636bb65d4cc
BhenRomano19 <ryazzbhen@gmail.com>
2026-09-01 11:49:01 +0800

    Add files via upload

---
+++ Activity5.py
@@ -0,0 +1,26 @@
+#operator
+# aritmetic 
+# - subtraction 
+# + addition 
+# * multiplication 
+# / division
+# % mudulus
+# ** exponent
+# // floor division 
+
+a = 55
+b = 5
+
+sum = a + b
+difference = a - b
+product = a * b
+quotient = a / b
+remainder = a % b
+power = a ** b
+
+print("The sum of ", a, "and", b, "is", sum)
+print("The difference of ", a, "and", b, "is", difference)
+print("The product of ", a, "and", b, "is", product)
+print("The quotient of ", a, "and", b, "is", quotient)
+print("The remainder of ", a, "and", b, "is", remainder)
+print("The power of ", a, "and", b, "is", power)
