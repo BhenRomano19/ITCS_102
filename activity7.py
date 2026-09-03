0434a8c2e6491a1801aaa75df49a41a762c2d7fc
BhenRomano19 <ryazzbhen@gmail.com>
2026-09-03 11:11:54 +0800

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,29 @@
+# assignment operators
+
+total = 0
+
+n1 = eval(input("The number of apples ----->"))
+n2 =  eval(input("The number of apples ----->"))
+n3 =  eval(input("The number of apples ----->"))
+n4 =  eval(input("The number of apples ----->"))
+n5 = eval(input("The number of apples ----->"))
+n6 = eval(input("The number of apples ----->"))
+n7 = eval(input("The number of apples ----->"))
+n8 = eval(input("The number of apples ----->"))
+n9 = eval(input("The number of apples ----->"))
+n10 = eval(input("The number of apples ----->"))
+
+total += n1
+total += n2
+total += n3
+total += n4
+total += n5
+total *= n6
+total *= n7
+total *= n8
+total *= n9
+total *= n10
+
+print("total is--->", total)
+
+ 
---
+++ activity8.py
@@ -0,0 +1,19 @@
+game = " " #blank space
+
+gm1 = input( "what are the games that you play ?")
+
+game += gm1 + " , "
+
+gm2 = input( "what else?")
+
+game += gm2 + " , "
+
+gm3 = input( "give me another one ")
+
+game += gm3 + " , "
+
+gm4 = input( "last one ")
+
+game += gm4 + " , "
+
+print(" The games that I played is?", game)
