ed6b8ccff80e80c8ef49263487ef4114a94a4c33
BhenRomano19 <ryazzbhen@gmail.com>
2026-09-08 11:12:07 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,25 @@
+# relational
+
+x = 8
+y = 8
+user1 = "okay"
+user2 = "Okay"
+
+#1
+print(x > y)
+
+#2 
+print(x < y)
+
+#3 
+print(x >= y)
+
+#4 
+print(x <= y)
+
+#5 
+print( x == y )
+print(user1 == user2)
+
+#6
+print(x != y)
