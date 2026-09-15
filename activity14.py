c456aa197428a89a4e8e05571e73c41ff70db066
BhenRomano19 <ryazzbhen@gmail.com>
2026-09-15 21:00:02 +0800

    Update activity14.py

--- activity14.py
+++ activity14.py
@@ -1,29 +1,38 @@
 print("====================================================")
+
 age = int(input("What is your age? =====>"))
 credit_score = int(input("What is your credit score? =====>"))
 is_employed = bool(input("Are you employed? =====>"))
 annual_income = float(input("What is your annual income? =====>"))
 has_collateral = bool(input("Do you have collateral? =====>"))
 base_rate = 0.0
+
 print("====================================================")
-if age >= 21 and is_employed == True: # tier 1
+
+if age >= 21 and is_employed == True: 
     print("You are eligible for a loan.")
-    if credit_score >= 750: #tier 2
+    if credit_score >= 750:
         if annual_income >= 100000:
             base_rate = 4.5
-            print("You are eligible for a lower interest rate of", base_rate, "%")
+            print("Your base rate is now", base_rate)
         else:
             base_rate = 5.0
-            print("base interest rate is", base_rate, "%")
-    elif 600 <= credit_score < 750: #tier 2
-        print("you credit score is less than 750, you are eligible for a loan but at a higher interest rate.")
+            print("Your base rate is now", base_rate )
+    elif credit_score >= 600 and credit_score < 750: 
+        print("Your credit score is less than 750")
         if has_collateral == True:
             base_rate = 7.0
-            print("You are eligible for a lower interest rate of", base_rate, "%")
-        elif annual_income >= 400000:
+            print("Your base rate is now", base_rate)
+        elif annual_income <= 40000:
             base_rate = 9.5
-            print("Your base rate is now ", base_rate, "%")
-
+            print("Your base rate is now ", base_rate)
+        else: 
+            base_rate = 8.0
+            print("Your base rate is now", base_rate)    
+    elif  credit_score <= 600:
+      print("credit score is to low") 
+else:
+    print("invalid")                     
   
 
 
